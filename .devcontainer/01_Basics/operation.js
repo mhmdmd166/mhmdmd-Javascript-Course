49bf6a4a773f6bf446ea2ba897a296ba943b6baf
mhmdmd166 <mhmdmdsrq@gmail.com>
2026-04-24 05:38:55 +0000

    operation

---
+++ .devcontainer/01_Basics/operation.js
@@ -0,0 +1,46 @@
+// Arithmetic Operands
+/*
+let a = 10, b = 5;
+
+a + b  // 15
+a - b  // 5
+a * b  // 50
+a / b  // 2
+a % b  // 0  (remainder)
+a ** b // power (10^5)
+*/
+
+//Assignmet Operator
+/* 
+let x = 10;
+
+x += 5  // x = x + 5 → 15
+x -= 2  // 13
+x *= 2  // 26
+x /= 2  // 13
+*/
+
+//Comparison Operator
+/*
+5 == "5"   // true  (type conversion)
+5 === "5"  // false (strict check)
+
+5 != 3     // true
+5 > 3      // true
+5 < 3      // false
+*/
+
+//logical operator
+/*
+true && false  // false (AND)
+true || false  // true  (OR)
+!true          // false (NOT)
+*/
+
+
+/*
+If numbers come first → normal math
+When string appears → everything after becomes string
+"1" + 2 + 2  // "122"
+1 + 2 + "2"  // "32"
+*/
