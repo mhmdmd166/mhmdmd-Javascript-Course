279232f8a517929228d46e7c9bf071dff7a69c8c
mhmdmd166 <mhmdmdsrq@gmail.com>
2026-04-28 18:00:23 +0000

    string in javascript

---
+++ .devcontainer/01_Basics/String.js
@@ -0,0 +1,10 @@
+let age = 50
+let name = 'Mudassar'
+console.log(`My name is ${name}.I am ${age} year old.`)
+let gameName ='TakenThree'
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(8))
+console.log(gameName.indexOf('l'))
+let newString=gameName.substring(0,5)
+let string =gameName.slice(4,8)//it also have negative values in arguent
